57466fdb2677dd31f9e11deb985965bcb6ac03c6
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-15 16:20:36 +1000

    created and aray to capture objects

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let taskList = [];
+
 const handleOnSubmit = (e) => {
   // const elm = document.getElementById("task")
   //   console.log(elm.value);
@@ -5,5 +7,11 @@ const handleOnSubmit = (e) => {
   const newForm = new FormData(e);
   const task = newForm.get("task");
   const hr = newForm.get("hr");
-  console.log(task, hr);
+
+  const obj = {
+    task,
+    hr,
+  };
+  taskList.push(obj);
+  console.log(taskList);
 };
