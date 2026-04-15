d171fc840f603d6042400b21393eead70258ea90
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-15 16:47:33 +1000

    added data to display

--- script.js
+++ script.js
@@ -13,5 +13,29 @@ const handleOnSubmit = (e) => {
     hr,
   };
   taskList.push(obj);
+  //   console.log(taskList);
+  displayEntryList();
+};
+
+//display entry list
+const displayEntryList = () => {
   console.log(taskList);
+  let str = "";
+  const entryelm = document.getElementById("entryList");
+  taskList.map((item, i) => {
+    str += ` <tr>
+                  <td>${i + 1}</td>
+                  <td>${item.task}</td>
+                  <td>${item.hr}</td>
+                  <td class="text-end">
+                    <button class="btn btn-danger">
+                      <i class="fa-solid fa-trash"></i>
+                    </button>
+                    <button class="btn btn-success">
+                      <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                  </td>
+                </tr>`;
+  });
+  entryelm.innerHTML = str;
 };
